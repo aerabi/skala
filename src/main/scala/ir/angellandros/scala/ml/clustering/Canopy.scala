66dc880c030a5d99d0cfbacc60c1c14ebff7355e
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2015-02-06 23:36:00 +0330

    Canopy example, with stopwords

--- src/main/scala/ir/angellandros/scala/ml/clustering/Canopy.scala
+++ src/main/scala/ir/angellandros/scala/ml/clustering/Canopy.scala
@@ -16,7 +16,7 @@ class CanopyDriver(t1: Double, t2: Double) {
 		for(v <- vs) set += v
 		for(v <- vs) {
 			count += 1
-			println(count)
+			print(count + " ")
 			if(set.contains(v)) {
 				par
 					.map{ x => (x, f(x, v)) }
@@ -27,18 +27,26 @@ class CanopyDriver(t1: Double, t2: Double) {
 			}
 		}
 		
+		println()
 		map
 	}
 	
-	def mergedRun[K](vs: List[KeyedVector[K]], f: (KeyedVector[K], KeyedVector[K]) => Double): HashMap[KeyedVector[K], KeyedVector[K]] = {
-		val partSize: Int = (Math.sqrt(vs.length)).ceil.toInt
+	def mergedRun[K](vs: List[KeyedVector[K]], f: (KeyedVector[K], KeyedVector[K]) => Double) = {
+		val partSize: Int = Math.sqrt(vs.length).ceil.toInt
 		
 		def runPartially(vs: List[KeyedVector[K]]): HashMap[KeyedVector[K], KeyedVector[K]] = {
 			if(vs.drop(partSize).isEmpty) run(vs.take(partSize), f)
 			else run(vs.take(partSize), f) ++ runPartially(vs.drop(partSize))
 		}
 		
-		runPartially(vs)
+		val map = runPartially(vs)
+		val centers = map.map(_._2).toSet
+		
+		println("Merge run")
+		val merge = run(centers, f)
+		
+		println("Merging")
+		map mapValues merge
 	}
 }
 
---
+++ src/test/scala/ir/angellandros/scala/ml/clustering/CanopyReuters.scala
@@ -0,0 +1,57 @@
+package ir.angellandros.scala.ml.clustering
+
+import java.io.File
+import scala.io._
+import scala.collection.mutable.Set
+
+import ir.angellandros.scala.collection.Implicits._
+import ir.angellandros.scala.collection._
+import ir.angellandros.scala.ml.clustering._
+	
+object CanopyReutersTest {
+	def main(args: Array[String]) {
+		// loading stop-words
+		val stopWords = Source.fromFile("src/main/resources/stopwords.txt").getLines.toSet
+	
+		// loading Reuters dataset
+		val dir = new File("B:/Documents/Dev/DataSets/Reuters/C50test")
+		val paths = dir.listFiles().flatMap(x => x.listFiles().map(_.getPath()))
+		val data = paths.map(x => Source.fromFile(x).mkString)
+		println("Loaded " + data.length + " files")
+		
+		def mkVector(text: String) = new KeyedVector(
+			text
+				.toLowerCase()
+				.replace("\t", " ")
+				.replace("\n", " ")
+				.replace("\"", "")
+				.replace("\'", "")
+				.replace(".", "")
+				.replace(",", "")
+				.replace(":", "")
+				.replace(";", "")
+				.split(" ")
+				.filter(!stopWords.contains(_))
+				.toList
+				.map(_ -> 1.0)
+				.reduceByKey(_+_)
+		)
+		
+		println("Making vectors")
+		// parallelized map
+		val vectors = data.par.map(mkVector).toList
+		
+		// canopy
+		val canopy = new CanopyDriver(0.95, 0.5)
+		println("Running canopy")
+		
+		val then = System.currentTimeMillis
+		val results = canopy.mergedRun(vectors, KeyedVectors.cosineDist[String])
+		val now = System.currentTimeMillis
+		println("Done in " + (now - then) + " milliseconds!")
+		
+		val centers = results.map(_._2).toSet
+		println(centers)
+		println(centers.toList.length)
+    }
+}
